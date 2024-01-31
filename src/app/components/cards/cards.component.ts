b1f70811d4dca816a6919c8c3061b14f483df9ab
Nicolas <nicolas.millar.millar@gmail.com>
2024-01-31 16:27:53 -0300

    reliza la peticion a la api y obtiene los datos de manera correcta

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -1,14 +1,25 @@
-import { Component, Input } from '@angular/core';
-import { Cards } from './../../models/cards.model'
+import { Component, OnInit, inject } from '@angular/core';
 import { CardComponent } from "../card/card.component";
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
     selector: 'app-cards',
     standalone: true,
     templateUrl: './cards.component.html',
     styleUrl: './cards.component.css',
-    imports: [CardComponent]
+    imports: [CardComponent, HttpClientModule]
 })
-export class CardsComponent {
-  
+export class CardsComponent implements OnInit{
+ http = inject(HttpClient)
+
+ ngOnInit(): void {
+   this.fetchData();
+ }
+
+ fetchData(){
+  this.http.get('http://127.0.0.1:5000/cards').
+  subscribe((data) =>{
+    console.log(data)
+  })
+ }
 }
